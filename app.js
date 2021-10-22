7715e5ed75bac53c1ebbfe5729ed27d083fc0d62
Bilal Ali <bilalali2@hotmail.com>
2021-10-22 19:46:12 -0400

    add item to todo list

---
+++ app.js
@@ -0,0 +1,28 @@
+// Function to generate a template list element 
+const createListTemplate = todoItem => {
+    const template =
+        `<li class="text-light list-group-item d-flex justify-content-between align-items-center">
+            <span>${todoItem}</span>
+            <i class="fas fa-trash delete"></i>
+        </li>`;
+
+    // Select todos class and append template to innerHTML
+    document.querySelector('.todos').innerHTML += template;
+}
+
+// Select HTML element of .add
+const addItem = document.querySelector(".add");
+
+// Action for page submit
+addItem.addEventListener('submit', event => {
+
+    // Prevent page reload upon submit
+    event.preventDefault();
+
+    // Retrieve value in input field
+    // Trim whitespace from beginning and end of text input
+    const newTodoItem = addItem.add.value.trim();
+
+    createListTemplate(newTodoItem);
+
+})
