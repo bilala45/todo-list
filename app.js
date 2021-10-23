b948e353939e00d1e497039d1bc22953aa0886d0
Bilal Ali <bilalali2@hotmail.com>
2021-10-22 21:21:34 -0400

    delete todo list items by clicking trash can

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// Function to generate a template list element 
+// Select HTML element of .add
+const addItem = document.querySelector(".add");
+
+// Select .todo container
+const todoContainer = document.querySelector('.todos');
+
+// Generate a list element template
 const createListTemplate = todoItem => {
     const template =
         `<li class="text-light list-group-item d-flex justify-content-between align-items-center">
@@ -7,11 +13,8 @@ const createListTemplate = todoItem => {
         </li>`;
 
     // Select todos class and append template to innerHTML
-    document.querySelector('.todos').innerHTML += template;
+    todoContainer.innerHTML += template;
 }
-
-// Select HTML element of .add
-const addItem = document.querySelector(".add");
 
 // Action for page submit
 addItem.addEventListener('submit', event => {
@@ -31,4 +34,18 @@ addItem.addEventListener('submit', event => {
         addItem.reset();
     }
 
+})
+
+// Action for clicking trash can icon
+todoContainer.addEventListener('click', event => {
+
+    // Retrieve HTML for item clicked
+    const elementHTML = event.target;
+
+    // Checks if item clicked is a trash can by checking for delete class
+    if (elementHTML.classList.contains('delete')) {
+
+        // Removes parent element of trash can element (todo list item)
+        elementHTML.parentElement.remove();
+    }
 })
